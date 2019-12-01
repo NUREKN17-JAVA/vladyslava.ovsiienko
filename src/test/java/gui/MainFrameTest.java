e1d536e69f479a969b56d509a7d13dd091705620
Ovsiienko Vladyslava <vladyslava.ovsiienko@nure.ua>
2019-12-01 22:52:45 +0200

    Create MainFrame and MainFrameTest classes

---
+++ src/test/java/gui/MainFrameTest.java
@@ -0,0 +1,23 @@
+package gui;
+
+import junit.extensions.jfcunit.JFCTestCase;
+import junit.extensions.jfcunit.JFCTestHelper;
+
+public class MainFrameTest extends JFCTestCase {
+	
+	private MainFrame mainFrame;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		setHelper(new JFCTestHelper());
+		mainFrame = new MainFrame();
+		mainFrame.setVisible(true);
+	}
+
+	protected void tearDown() throws Exception {
+		mainFrame.setVisible(false);
+		getHelper().cleanUp(this);
+		super.tearDown();
+	}
+
+}
