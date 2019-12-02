c64034500605aa29b3f1285d5484a0e2590ff985
Ovsiienko Vladyslava <vladyslava.ovsiienko@nure.ua>
2019-12-02 02:19:43 +0200

    creation of a test of the main window and control panel and their implementation(step 3-6)

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -1,12 +1,51 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
+	
+	private static final int FRAME_HEIGHT = 600;
+	private static final int FRAME_WIDTH = 800;
+	private JPanel contentPanel;
+	private JPanel browsePanel;
+	
+	public MainFrame(){
+		super();
+		initialize();
+	}
+	
+	private void initialize() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(FRAME_WIDTH,FRAME_HEIGHT);
+		this.setTitle("”правление пользовател€ми");
+		this.setContentPane(getContentPanel());
+		
+	}
+
+	private JPanel getContentPanel() {
+		if(contentPanel == null){
+			contentPanel = new JPanel();
+			contentPanel.setLayout(new BorderLayout());
+			contentPanel.add(getBrowsePanel(),BorderLayout.CENTER);
+		}
+		return contentPanel;
+	}
+
+	private JPanel getBrowsePanel() {
+		if (browsePanel == null){
+			browsePanel = new BrowsePanel(this);
+		}
+		return browsePanel;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
 	}
 
 }
--- src/test/java/gui/MainFrameTest.java
+++ src/test/java/gui/MainFrameTest.java
@@ -1,7 +1,14 @@
 package gui;
 
+import java.awt.Component;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
+import junit.extensions.jfcunit.finder.NamedComponentFinder;
 
 public class MainFrameTest extends JFCTestCase {
 	
@@ -20,4 +27,21 @@ public class MainFrameTest extends JFCTestCase {
 		super.tearDown();
 	}
 
+	private Component find(Class componentClass,String name){
+		NamedComponentFinder finder;
+		finder = new NamedComponentFinder(componentClass, name);
+		finder.setWait(0);
+		Component component = finder.find(mainFrame,0);
+		assertNotNull("Could not find component '" + name + "'",component);
+		return component;
+	}
+	
+	public void testBrowseControls(){
+		find(JPanel.class,"browsePanel");
+		find(JTable.class,"userTable");
+		find(JButton.class,"addButton");
+		find(JButton.class,"editButton");
+		find(JButton.class,"deleteButton");
+		find(JButton.class,"detailsButton");
+	}
 }
