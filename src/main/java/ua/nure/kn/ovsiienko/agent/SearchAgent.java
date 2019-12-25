882164d18469aec2c8ef4f9a41cde015b49da3ff
Ovsiienko Vladyslava <vladyslava.ovsiienko@nure.ua>
2019-12-25 21:59:16 +0200

    adding agent behavior and registering with the jade platform service

--- src/main/java/ua/nure/kn/ovsiienko/agent/SearchAgent.java
+++ src/main/java/ua/nure/kn/ovsiienko/agent/SearchAgent.java
@@ -6,6 +6,10 @@ import ua.nure.kn.ovsiienko.db.DaoFactory;
 import ua.nure.kn.ovsiienko.db.DatabaseException;
 import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 public class SearchAgent extends Agent {
 
@@ -16,10 +20,30 @@ protected void setup() {
 	super.setup();
 	System.out.println(getAID().getName()+ " started");
 	
+	DFAgentDescription description = new DFAgentDescription();
+	description.setName(getAID());
+	ServiceDescription serviceDescription = new ServiceDescription();
+	serviceDescription.setName("JADE-searching");
+	serviceDescription.setType("searching");
+	description.addServices(serviceDescription);
+	try{
+		DFService.register(this,description);
+	}catch(FIPAException e){
+		e.printStackTrace();
+	}
+	
+	addBehaviour(new RequestServer());
+	
 }
 
 	protected void takeDown() {
 		System.out.println(getAID().getName() + " terminated");
+		try{
+			DFService.deregister(this);
+		} catch(FIPAException e){
+			e.printStackTrace();
+			
+		}
 		super.takeDown();
 	}
 	
